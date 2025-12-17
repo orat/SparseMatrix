5c62dc0556538b63061969bc89fe9e3c8f4c8427
Oliver Rettig <Oliver.Rettig@orat.de>
2025-12-17 12:26:12 +0100

    add fromValues(double[] values) into SparseDoubleColumnvector

--- src/main/java/de/orat/math/sparsematrix/SparseDoubleColumnVector.java
+++ src/main/java/de/orat/math/sparsematrix/SparseDoubleColumnVector.java
@@ -1,5 +1,8 @@
 package de.orat.math.sparsematrix;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Oliver Rettig (Oliver.Rettig@orat.de)
  */
@@ -123,5 +126,28 @@ public class SparseDoubleColumnVector extends /*DoubleVector*/ SparseDoubleMatri
             result[row[i]] = data[i];
         }
         return result;
+    }
+    
+    public static SparseDoubleColumnVector fromValues(double[] values) {
+        int bladesCount = values.length;
+        List<Integer> nonzeroPositions = new ArrayList<>(values.length);
+        List<Double> nonzeroValues = new ArrayList<>(values.length);
+
+        for (int i = 0; i < values.length; i++) {
+            double value = values[i];
+            if (value == 0d) {
+                continue;
+            }
+            nonzeroPositions.add(i);
+            nonzeroValues.add(value);
+        }
+
+        int[] nonzerosPositionsArray = nonzeroPositions.stream().mapToInt(Integer::intValue).toArray();
+        double[] nonzeroValuesArray = nonzeroValues.stream().mapToDouble(Double::doubleValue).toArray();
+
+        //var sparsity = new CGAMultivectorSparsity(nonzerosPositionsArray);
+        var sparsity = new ColumnVectorSparsity(bladesCount, nonzerosPositionsArray);
+        //return new SparseCGAColumnVector(sparsity, nonzeroValuesArray);
+        return new SparseDoubleColumnVector(sparsity, nonzeroValuesArray);
     }
 }
